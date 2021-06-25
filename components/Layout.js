481d5c46700bcb95103ccd1f54fe17f23ed97255
kimutai-codes <allankimutaikoech@gmail.com>
2021-06-25 14:38:29 +0300

    netlify cms and changed one line in package.json

--- components/Layout.js
+++ components/Layout.js
@@ -1,29 +1,35 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
   return (
-    <div className="layout">
-      <Navbar />
-      <div className="mwili">{children}</div>
-      <Footer />
-      <style jsx>
-        {`
-          //think this should be in the body
-          //should separate body from nav
-          //where is the boundary
-          .mwili {
-            // color: red;
-          }
-          @media screen and (min-width: 700px) {
-            .layout {
-              max-width: 65%;
-              margin: 0 auto;
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <div className="layout">
+        <Navbar />
+        <div className="mwili">{children}</div>
+        <Footer />
+        <style jsx>
+          {`
+            //think this should be in the body
+            //should separate body from nav
+            //where is the boundary
+            .mwili {
+              // color: red;
             }
-          }
-        `}
-      </style>
-    </div>
+            @media screen and (min-width: 700px) {
+              .layout {
+                max-width: 65%;
+                margin: 0 auto;
+              }
+            }
+          `}
+        </style>
+      </div>
+    </>
   );
 };
 
