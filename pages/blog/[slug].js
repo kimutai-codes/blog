37f6ddfb913d5a731ab995764fed7901f02fac12
kimutai-codes <allankimutaikoech@gmail.com>
2021-07-30 17:29:12 +0300

    image optimized

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,80 +1,92 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import Head from "next/head";
-import marked from "marked";
-import { format } from "date-fns";
-import moment from "moment";
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import Head from 'next/head';
+import marked from 'marked';
+import { format } from 'date-fns';
+import Image from 'next/image';
+import moment from 'moment';
 
 export const getStaticPaths = async () => {
-  //get paths
-  //this will return an array
-  const files = fs.readdirSync("posts");
+	//get paths
+	//this will return an array
+	const files = fs.readdirSync('posts');
 
-  const paths = files.map((fileName) => ({
-    params: {
-      // get id of the file to be used in path
-      slug: fileName.replace(".md", ""),
-    },
-  }));
+	const paths = files.map((fileName) => ({
+		params: {
+			// get id of the file to be used in path
+			slug: fileName.replace('.md', ''),
+		},
+	}));
 
-  //an array that contains a params obj that in turn contains id obj
-  // params: {
-  //   slug: "slug.md",
-  // },
-  return {
-    paths,
-    fallback: false,
-  };
+	//an array that contains a params obj that in turn contains id obj
+	// params: {
+	//   slug: "slug.md",
+	// },
+	return {
+		paths,
+		fallback: false,
+	};
 };
 
 //function to fetch the actual contenet of the post
 //takes an argument of the parameter we need to fetch data for
 export const getStaticProps = async ({ params: { slug } }) => {
-  //read the contents of the file
-  //"make the file" using path.join with the slug from above then add the file extention
-  const markdownWithMetadata = fs
-    .readFileSync(path.join("posts", slug + ".md"))
-    .toString();
+	//read the contents of the file
+	//"make the file" using path.join with the slug from above then add the file extention
+	const markdownWithMetadata = fs
+		.readFileSync(path.join('posts', slug + '.md'))
+		.toString();
 
-  const parsedMarkdown = matter(markdownWithMetadata);
-  const htmlString = marked(parsedMarkdown.content);
-  const rawDate = parsedMarkdown.data.date.toString();
-  //date_fns working too
-  const parsedDate = format(new Date(rawDate), "do MMM yyyy");
-  // meoment JS working
-  // const parsedDate = moment(rawDate).format("Do MMM YYYY");
+	const parsedMarkdown = matter(markdownWithMetadata);
+	const htmlString = marked(parsedMarkdown.content);
+	const rawDate = parsedMarkdown.data.date.toString();
+	//date_fns working too
+	const parsedDate = format(new Date(rawDate), 'do MMM yyyy');
+	// meoment JS working
+	// const parsedDate = moment(rawDate).format("Do MMM YYYY");
 
-  //return an object with props inside of it
-  return {
-    props: {
-      htmlString,
-      data: parsedMarkdown.data,
-      parsedDate,
-    },
-  };
+	//return an object with props inside of it
+	return {
+		props: {
+			htmlString,
+			data: parsedMarkdown.data,
+			parsedDate,
+		},
+	};
 };
 
 //this is the template that also makes dinamic routes
 const Post = ({ htmlString, data, parsedDate }) => {
-  return (
-    <>
-      <Head>
-        <title>{data.title}</title>
-        <meta name="description" content={data.description} />
-      </Head>
-      <span className="blogHeader">
-        <h1>{data.title}</h1>
-        <span className="desc">{data.description}</span>
-        <span className="date">{parsedDate}</span>
-      </span>
+	console.log(data);
+	return (
+		<>
+			<Head>
+				<title>{data.title}</title>
+				<meta name='description' content={data.description} />
+			</Head>
 
-      <div
-        dangerouslySetInnerHTML={{ __html: htmlString }}
-        className="blogContent"
-      />
+			<span className='blogHeader'>
+				<h1>{data.title}</h1>
+				<span className='desc'>{data.description}</span>
+				<span className='date'>{parsedDate}</span>
+				{/* <img src={`/${data.thumbnail}`} alt='' /> */}
+			</span>
 
-      <style jsx>{`
+			<Image
+				src={`/${data.thumbnail}`}
+				width={1080}
+				height={610}
+				alt=''
+				priority
+			/>
+
+			<div
+				dangerouslySetInnerHTML={{ __html: htmlString }}
+				className='blogContent'
+			/>
+
+			<style jsx>{`
         .blogHeader {
           margin-bottom: 0;
         }
@@ -101,8 +113,8 @@ const Post = ({ htmlString, data, parsedDate }) => {
           } */}
         }
       `}</style>
-    </>
-  );
+		</>
+	);
 };
 
 export default Post;
