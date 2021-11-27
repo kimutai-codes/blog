839a961c1b8303c837e4d31ca652285fb40ab91b
kimutai-codes <allankimutaikoech@gmail.com>
2021-11-27 15:24:22 +0300

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
-import Layout from "../components/Layout";
-import "../styles/globals.css";
+import Layout from '../components/Layout';
+import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} className="child" />
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Script
+				strategy='lazyOnload'
+				src={`https://www.googletagmanager.com/gtag/js?id=G-R8VRLRB0H9`}
+			/>
+
+			<Script strategy='lazyOnload'>
+				{`
+        window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-R8VRLRB0H9')
+        `}
+			</Script>
+
+			<Component {...pageProps} className='child' />
+		</Layout>
+	);
 }
 
 export default MyApp;
