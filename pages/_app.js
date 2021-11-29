92f22e0d8096da0a6e170c86a2ebbb40d6667664
kimutai-codes <allankimutaikoech@gmail.com>
2021-11-29 12:57:10 +0300

    can now edit

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,38 @@
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 import Script from 'next/script';
+import dynamic from 'next/dynamic';
+import { TinaEditProvider } from 'tinacms/dist/edit-state';
+const TinaCMS = dynamic(() => import('tinacms'), { ssr: false });
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Script
-				strategy='lazyOnload'
-				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.MY_GOOGLE_ANNALYTICS}`}
-			/>
+		<TinaEditProvider
+			editMode={
+				<TinaCMS {...pageProps}>
+					{(livePageProps) => <Component {...livePageProps} />}
+				</TinaCMS>
+			}
+		>
+			<Layout>
+				<Script
+					strategy='lazyOnload'
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.MY_GOOGLE_ANNALYTICS}`}
+				/>
 
-			<Script strategy='lazyOnload'>
-				{`
+				<Script strategy='lazyOnload'>
+					{`
         window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config',${process.env.MY_GOOGLE_ANNALYTICS})
         `}
-			</Script>
+				</Script>
 
-			<Component {...pageProps} className='child' />
-		</Layout>
+				<Component {...pageProps} className='child' />
+			</Layout>
+		</TinaEditProvider>
 	);
 }
 
